518ea2479feb7200b6a5ee9c3142798910c56628
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2023-12-21 05:21:42 +0600

    refactor: refactor this code for show proper error message

--- server/modules/Employee/validator/index.js
+++ server/modules/Employee/validator/index.js
@@ -1,7 +1,18 @@
 const Joi = require("joi");
 
 exports.employeeSchema = Joi.object({
-    firstName: Joi.string().min(3).required(),
-    lastName: Joi.string().min(3).required(),
-    email: Joi.string().email().required(),
+    firstName: Joi.string().min(3).required().messages({
+        "string.empty": `{{#label}} cannot be an empty field`,
+        "string.min": `{{#label}} should have a minimum length of {{#limit}}`,
+        "any.required": `{{#label}} is a required field`,
+    }),
+    lastName: Joi.string().min(3).required().messages({
+        "string.empty": `{{#label}} cannot be an empty field`,
+        "string.min": `{{#label}} should have a minimum length of {{#limit}}`,
+        "any.required": `{{#label}} is a required field`,
+    }),
+    email: Joi.string().email().required().messages({
+        "string.email": `{{#label}} should be a valid email address`,
+        "any.required": `{{#label}} is a required field`,
+    }),
 });
