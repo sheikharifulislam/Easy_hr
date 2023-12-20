23f022f8ebf4efe9e62505d56bd8a73882799e80
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2023-12-21 04:12:36 +0600

    feat: have added some configuration on server.js file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+var cors = require("cors");
 const app = express();
+const { connectDb } = require("./db");
+const routes = require("./routes");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1", routes);
 
 app.get("/health", (req, res) => {
     res.status(200).json({
@@ -8,7 +15,18 @@ app.get("/health", (req, res) => {
     });
 });
 
+app.use((err, req, res, next) => {
+    console.log("from global error handler");
+    const message = err.message && err.status ? err.message : "Server Error Occured";
+    const status = err.status ? err.status : 500;
+    console.log(err.message);
+    res.status(status).json({
+        message,
+    });
+});
+
 const port = process.env.PORT;
-app.listen(port, () => {
+app.listen(port, async () => {
+    await connectDb();
     console.log(`Server is listen on ${port}`);
 });
